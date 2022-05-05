92cd8d2270b63e6088e94e4f7181416159362ad9
brunash <bobazso@yahoo.com>
2022-05-05 15:43:31 -0400

    added sign in page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import { Container } from '@material-ui/core';
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
+import Auth from "./components/Auth/Auth";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 
 const App = () => {
     
     return (
+        <BrowserRouter>
         <Container maxWidth='lg'>
             <NavBar/>
-            <Home/>
+            <Switch>
+                <Route path='/' exact component={Home}/>
+                <Route path='/auth' component={Auth}/>
+            </Switch>
         </Container>
+        </BrowserRouter>
     )
 }
 
